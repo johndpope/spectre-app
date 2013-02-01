e933dd847803805e5218d5221cbd3be8b265c462
Nitin Punjabi <nptoronto@yahoo.com>
2013-02-01 13:38:55 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubleTake::Application.config.secret_token = 'c9ffc4db4baae3dec0dc6e3c17797ec96bd10591461bd0f7e16f575056ef8e28fbc7c2624bfd8bf7e142990e9ce88d48a210f4aa99c257315ed6485cd05e434d'
