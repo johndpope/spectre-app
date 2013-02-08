050fdf018cfde05772189fae76f4ae06f44df634
Nitin Punjabi <nptoronto@yahoo.com>
2013-02-08 13:39:06 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,11 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Officer", email: "officer@spectre.com",
+  before do
+    @user = User.new(name: "Officer", email: "officer@spectre.com",
                             password: "spectre",
-                            password_confirmation: "spectre") }
+                            password_confirmation: "spectre")
+  end
 
   subject { @user }
 
@@ -24,6 +26,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -118,5 +121,10 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
