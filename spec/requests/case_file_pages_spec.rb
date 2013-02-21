10e481423a80aa75d7c0733f9c7e0de57c1d3fe3
Nitin Punjabi <nptoronto@yahoo.com>
2013-02-21 15:10:01 +0100

    Render available case actions menu

--- spec/requests/case_file_pages_spec.rb
+++ spec/requests/case_file_pages_spec.rb
@@ -76,41 +76,70 @@ describe "CaseFilePages" do
 
       it { should have_selector('h4', text: 'None Found') }
     end
-  end
 
-  describe "show" do
+    describe "when viewing my own case" do
 
-    let(:user) { FactoryGirl.create(:user) }
-    let!(:case_file) { FactoryGirl.create(:layering, user: user) }
+      before { visit case_file_path(case_file1) }
+      
+      it { should have_content(case_file1.id.to_s) }
+      it { should have_content(user1.name) }
+      it { should have_content('Open') }
 
-    before do
-      sign_in user
-      visit case_file_path(case_file)
-    end
+      it { should have_content('Available Case Actions') }
+      it { should have_link('Comment') }
+      it { should have_link('Transfer') }
+      it { should have_link('Close') }
 
-    it { should have_content(case_file.id.to_s) }
-    it { should have_content(user.name) }
-    it { should have_content('Open') }
+      it { should_not have_link('Confirm Close') }
+      it { should_not have_link('Reject Close') }
+      it { should_not have_link('Reopen') }
 
-    describe "when a case goes into pending close state" do
-      before do
-        case_file.update_attribute(:open, true)
-        case_file.update_attribute(:pending_close, true)
-        visit case_file_path(case_file)
+      describe "when a case goes into pending close state" do
+        before do
+          case_file1.update_attribute(:open, true)
+          case_file1.update_attribute(:pending_close, true)
+          visit case_file_path(case_file1)
+        end
+
+        it { should have_content('Awaiting close confirmation') }
+
+        it { should have_link('Confirm Close') }
+        it { should have_link('Reject Close') }
+
+        it { should_not have_link('Comment') }
+        it { should_not have_link('Transfer') }
+        it { should_not have_link('Reopen') }
       end
 
-      it { should have_content('Awaiting close confirmation') }
-    end
+      describe "when a case goes into closed state" do
+        before do
+          case_file1.update_attribute(:open, false)
+          case_file1.update_attribute(:pending_close, true)
+          visit case_file_path(case_file1)
+        end
 
-    describe "when a case goes into closed state" do
-      before do
-        case_file.update_attribute(:open, false)
-        case_file.update_attribute(:pending_close, true)
-        visit case_file_path(case_file)
+        it { should have_content('Closed') }
+
+        it { should have_link('Reopen') }
+        
+        it { should_not have_link('Comment') }
+        it { should_not have_link('Transfer') }
+        it { should_not have_link('Close') }
+        it { should_not have_link('Confirm Close') }
+        it { should_not have_link('Reject Close') }
+
       end
-
-      it { should have_content('Closed') }
     end
 
+    describe "when viewing another user's case" do
+      before { visit case_file_path(case_file2) }
+
+        it { should_not have_link('Reopen') }      
+        it { should_not have_link('Comment') }
+        it { should_not have_link('Transfer') }
+        it { should_not have_link('Close') }
+        it { should_not have_link('Confirm Close') }
+        it { should_not have_link('Reject Close') }
+    end
   end
 end
