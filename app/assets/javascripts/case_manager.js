76aaae80908d0e75914be569732fbe5b0949fb6b
Nitin Punjabi <nptoronto@yahoo.com>
2013-02-22 09:47:46 +0100

    Store orders in accordian

--- app/assets/javascripts/case_manager.js
+++ app/assets/javascripts/case_manager.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
+  $("#orders").unbind("show");
+  $("#orders").unbind("hide");
+
+  $("#case_manager_tabs").click(function(e) {
+    $('a[href=#history]').tab('show');
+  });
+
+  $('#orders').on('show', function () {
+    $("#toggle").text("Hide Orders");
+  })
+
+  $('#orders').on('hide', function () {
+    $("#toggle").text("Show Orders");
+  })
+
   if (location.hash !== '') $('a[href="' + location.hash + '"]').tab('show');
   return $('a[data-toggle="tab"]').click(function(e) {
     return location.hash = $(e.target).attr('href').substr(1);
   });
+
 });
 
-$(document).ready(function() {
-  $("#case_manager_tabs").click(function(e) {
-    $('a[href=#history]').tab('show');
-  });
-});
+
+
