f6e44018790fb26d1f1737db15be67c56c6c9c15
Nitin Punjabi <nptoronto@yahoo.com>
2013-03-19 15:11:33 +0100

    Expand test coverage of individual case file type pages

--- spec/factories.rb
+++ spec/factories.rb
@@ -82,6 +82,84 @@ FactoryGirl.define do
     participants "Acme Trading"
     symbol "SANp"
     user
+  end 
+
+  factory :wash_trading_case do
+    type "WashTradingCase"
+    name "Wash trading"
+    content [
+              {
+                "sender-comp-id" => "VENUE_X",
+                "target-sub-id" => "CUSTOMER_X",
+                "leaves-qty" => 0,
+                "liquidity-flag" => "added",
+                "last-share" => 100,
+                "price" => 1.04,
+                "avg-price" => 1.04,
+                "transact-time" => "20130228-10:47:07",
+                "event-timestamp" => 1362044827000,
+                "order-status" => "filled",
+                "last-price" => 1.04,
+                "side" => "buy",
+                "order-qty" => 100,
+                "symbol" => "SANp",
+                "order-type" => "limit",
+                "customer-name" => "Acme Trading",
+                "order-id" => "2503",
+                "cumulative-qty" => 100,
+                "client-order-id" => "10471028479",
+                "target-comp-id" => "CUSTOMER"
+              }
+            ].to_json()
+    detection_time Time.now.utc.iso8601
+    participants "Acme Trading"
+    symbol "SANp"
+    user
+  end
+
+  factory :large_unfilled_order_case do
+    type "LargeUnfilledOrderCase"
+    name "Large unfilled order"
+    content Hash["order",
+                  {
+                    "sender-comp-id" => "VENUE_X",
+                    "target-sub-id" => "CUSTOMER_X",
+                    "leaves-qty" => 0,
+                    "last-share" => 0,
+                    "price" => 1.0,
+                    "avg-price" => 0.0,
+                    "transact-time" => "20130228-10:47:11",
+                    "order-status" => "canceled",
+                    "last-price" => 0.0,
+                    "side" => "buy",
+                    "order-qty" => 1000000,
+                    "symbol" => "SANp",
+                    "order-type" => "limit",
+                    "customer-name" => "Acme Trading",
+                    "order-id" => "2499",
+                    "cumulative-qty" => 0,
+                    "client-order-id" => "10471028480",
+                    "target-comp-id" => "CUSTOMER"
+                  },
+                  "avg-order-size", 100].to_json()
+    detection_time Time.now.utc.iso8601
+    participants "Acme Trading"
+    symbol "SANp"
+    user
+  end
+
+  factory :low_trade_to_cancel_ratio_case do
+    type "LowTradeToCancelRatioCase"
+    name "Low trade-to-cancel ratio"
+    content Hash["fills", 5,
+                 "cancels", 40,
+                 "ratio", 0.125,
+                 "symbol", "SANp",
+                 "threshold", 0.3].to_json()
+    detection_time Time.now.utc.iso8601
+    participants "Acme Trading"
+    symbol "SANp"
+    user
   end
 
   factory :bid_layering_monitor do
@@ -90,6 +168,7 @@ FactoryGirl.define do
               "info", "Layering on the bid description"].to_json
     settings Hash["number-of-new-bids", 4,
                   "number-of-cancels", 4,
+                  "bids-should-be-in-increasing-order", true,
                   "length-of-monitoring-window", 5].to_json
     active true
   end
@@ -100,6 +179,7 @@ FactoryGirl.define do
               "info", "Layering on the bid description"].to_json
     settings Hash["number-of-new-bids", 4,
                   "number-of-cancels", 4,
+                  "asks-should-be-in-decreasing-order", true,
                   "length-of-monitoring-window", 5].to_json
     active true
   end
--- spec/requests/case_file_pages_spec.rb
+++ spec/requests/case_file_pages_spec.rb
@@ -7,7 +7,8 @@ describe "CaseFilePages" do
   describe "index" do
 
     let(:user) { FactoryGirl.create(:user) }
-    let!(:case_file) { FactoryGirl.create(:bid_layering_case, user: user) }
+    let!(:bid_layering_case) { FactoryGirl.create(:bid_layering_case,
+                                                  user: user) }
 
     before do
       sign_in user
@@ -18,7 +19,7 @@ describe "CaseFilePages" do
     it { should have_selector('h1', text: 'All Open Cases') }
     
 
-    it { should have_selector('td', text: case_file.id.to_s) }
+    it { should have_selector('td', text: bid_layering_case.id.to_s) }
     it { should have_selector('td', text: 'Layering on the bid') }
     it { should have_selector('td', text: 'Acme') }
     it { should have_selector('td', text: 'SANp') }
@@ -29,9 +30,10 @@ describe "CaseFilePages" do
 
     describe "when someone takes action on a case" do
       let!(:action) { user.actions.create!(type: "Open",
-                                           desc: "opened case #{case_file.id}",
+                                           desc: "opened case
+                                                  #{bid_layering_case.id}",
                                            content: "Open action",
-                                           case_file_id: case_file.id) }
+                                           case_file_id: bid_layering_case.id) }
 
       before { visit case_files_path }
       it { should have_selector('a', :'data-original-title' => 'No Actions Found' ) }
@@ -39,8 +41,8 @@ describe "CaseFilePages" do
 
     describe "when there are no open cases" do
       before do
-        case_file.update_attribute(:open, false)
-        case_file.update_attribute(:pending_close, true)
+        bid_layering_case.update_attribute(:open, false)
+        bid_layering_case.update_attribute(:pending_close, true)
         visit case_files_path
       end
       
@@ -175,7 +177,7 @@ describe "CaseFilePages" do
             visit my_cases_path
           end
 
-          it { should have_selector('a', :'data-content' => 'requested close' ) }
+          it { should have_selector('a', :'data-content' => 'requested close' )}
           it { should have_selector('td', text: 'Awaiting close confirmation') }
           it { should have_selector('td', text: user2.name) }
 
@@ -222,6 +224,115 @@ describe "CaseFilePages" do
         it { should_not have_link('Close') }
         it { should_not have_link('Confirm Close') }
         it { should_not have_link('Reject Close') }
+    end
+  end
+
+  describe "when viewing individual case files types" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:bid_layering_case) { FactoryGirl.create(:bid_layering_case,
+                                                  user: user) }
+    let!(:ask_layering_case) { FactoryGirl.create(:ask_layering_case,
+                                                  user: user) }
+    let!(:wash_trading_case) { FactoryGirl.create(:wash_trading_case,
+                                                  user: user) }
+    let!(:large_unfilled_order_case) { FactoryGirl.create(
+                                         :large_unfilled_order_case,
+                                         user: user) }
+    let!(:low_trade_to_cancel_case) { FactoryGirl.create(
+                                        :low_trade_to_cancel_ratio_case,
+                                        user: user) }
+
+    before { sign_in user }
+
+    describe "and their summaries" do
+      before { visit case_file_path(bid_layering_case) }
+
+      it { should have_content("You are viewing Case " +
+                               "#{ bid_layering_case.id }") }
+      it { should have_content("Case Officer: You ") }
+
+      it { should have_content("Case Id: #{ bid_layering_case.id }") }
+      it { should have_content("Status: Open") }
+      it { should have_content("Incident: #{ bid_layering_case.name }") }
+      it { should have_content("Symbol: #{ bid_layering_case.symbol }") }
+      it { should have_content("Incident Participants: " +
+                               "#{ bid_layering_case.participants }") }
+      it { should have_content("Case Officer: " +
+                               "#{ User.find(
+                                     bid_layering_case.user_id).name }")}
+      it { should have_content("Detection Time: " +
+                               "#{ bid_layering_case.detection_time.to_s }") }
+
+      it { should have_content("Case History") }
+      it { should have_link("View whole history") }
+    end
+
+    describe "and bid layering case content" do
+      before { visit case_file_path(bid_layering_case) }
+
+      it { should have_selector('th', text: 'Participant') }
+      it { should have_selector('th', text: 'Order State') }
+      it { should have_selector('th', text: 'Side') }
+      it { should have_selector('th', text: 'Size') }
+      it { should have_selector('th', text: 'Symbol') }
+      it { should have_selector('th', text: 'Price') }
+      it { should have_selector('th', text: 'Event Time') }
+      it { should have_selector('th', text: 'Client Order Id') }
+      it { should have_link('+') }
+      it { should have_link('Hide Events') }
+    end
+
+    describe "and ask layering case content" do
+      before { visit case_file_path(ask_layering_case) }
+
+      it { should have_selector('th', text: 'Participant') }
+      it { should have_selector('th', text: 'Order State') }
+      it { should have_selector('th', text: 'Side') }
+      it { should have_selector('th', text: 'Size') }
+      it { should have_selector('th', text: 'Symbol') }
+      it { should have_selector('th', text: 'Price') }
+      it { should have_selector('th', text: 'Event Time') }
+      it { should have_selector('th', text: 'Client Order Id') }
+      it { should have_link('+') }
+      it { should have_link('Hide Events') }
+    end
+
+    describe "and low trade-to-cancel ratio content" do
+      before { visit case_file_path(low_trade_to_cancel_case) }
+
+      it { should have_selector('th', text: 'Symbol') }
+      it { should have_selector('th', text: 'Trades') }
+      it { should have_selector('th', text: 'Cancels') }
+      it { should have_selector('th', text: 'Ratio') }
+    end
+
+    describe "and large unfilled order case content" do
+      before { visit case_file_path(large_unfilled_order_case) }
+
+      it { should have_selector('th', text: 'Participant') }
+      it { should have_selector('th', text: 'Order State') }
+      it { should have_selector('th', text: 'Side') }
+      it { should have_selector('th', text: 'Size Remaining') }
+      it { should have_selector('th', text: 'Symbol') }
+      it { should have_selector('th', text: 'Price') }
+      it { should have_selector('th', text: 'Transact Time') }
+      it { should have_selector('th', text: 'Client Order Id') }
+      it { should have_link('+') }
+    end
+
+    describe "and wash trading case content" do
+      before { visit case_file_path(wash_trading_case) }
+      
+      it { should have_selector('th', text: 'Participant') }
+      it { should have_selector('th', text: 'Order State') }
+      it { should have_selector('th', text: 'Side') }
+      it { should have_selector('th', text: 'Size') }
+      it { should have_selector('th', text: 'Symbol') }
+      it { should have_selector('th', text: 'Price') }
+      it { should have_selector('th', text: 'Liquidity Flag') }
+      it { should have_selector('th', text: 'Transact Time') }
+      it { should have_selector('th', text: 'Client Order Id') }
+      it { should have_link('+') }
     end
   end
 end
